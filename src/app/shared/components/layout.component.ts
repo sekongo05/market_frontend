7e1a93bb333348c9670f1bc3a2fd1e77fa4aac69
mosek <sekongomoussa77@gmail.com>
2026-05-19 15:37:13 +0000

    update

--- src/app/shared/components/layout.component.ts
+++ src/app/shared/components/layout.component.ts
@@ -1,5 +1,6 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, ChangeDetectionStrategy, ChangeDetectorRef, OnInit, OnDestroy } from '@angular/core';
+import { Router, NavigationStart, NavigationEnd, NavigationCancel, NavigationError, RouterModule } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { NavbarComponent } from './navbar.component';
 import { SupportWidgetComponent } from './support-widget.component';
 import { ToastComponent } from './toast.component';
@@ -14,8 +15,35 @@ import { SdmLogoComponent } from './logo.component';
   styleUrls: ['./layout.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LayoutComponent {
-  constructor(private authService: AuthService) {}
+export class LayoutComponent implements OnInit, OnDestroy {
+  navigating = false;
+  private routerSub?: Subscription;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private cdr: ChangeDetectorRef,
+  ) {}
+
+  ngOnInit(): void {
+    this.routerSub = this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        this.navigating = true;
+        this.cdr.markForCheck();
+      } else if (
+        event instanceof NavigationEnd ||
+        event instanceof NavigationCancel ||
+        event instanceof NavigationError
+      ) {
+        this.navigating = false;
+        this.cdr.markForCheck();
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.routerSub?.unsubscribe();
+  }
 
   get isManager(): boolean {
     const role = this.authService.getCurrentUser()?.role;
